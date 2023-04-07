bd7e50163dd21131d705b7d9829ad6fce34bc41b
Shu-Ye Lin <67604195+shuyelin06@users.noreply.github.com>
2023-04-07 19:11:49 -0400

    Project Setup

---
+++ src/engine/Main.java
@@ -0,0 +1,54 @@
+package engine;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Main extends StateBasedGame 
+{
+	// GameState IDs
+	public static final int EXAMPLE_ID = 0;
+      
+    // BasicGameStates 
+    private BasicGameState example;
+    
+    private static AppGameContainer appgc;
+    
+    
+	public Main(String name) { 
+		super(name); 
+		
+		this.example = new ExampleScene(EXAMPLE_ID);
+	}
+
+	public static int getScreenWidth() { return appgc.getScreenWidth(); }
+	public static int getScreenHeight() { return appgc.getScreenHeight(); }
+	
+	public static int getFPS() { return appgc.getFPS(); }
+	
+	public void initStatesList(GameContainer gc) throws SlickException 
+	{
+		
+		addState(example);
+	}
+
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			appgc = new AppGameContainer(new Main("Bitcamp 2023"));
+			System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+			
+			appgc.setDisplayMode(appgc.getScreenWidth(), appgc.getScreenHeight(), false);
+			appgc.setTargetFrameRate(60);
+			
+			appgc.start();
+			appgc.setVSync(true);
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
