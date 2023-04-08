e498134d59c50bc7ed776db6e6724ec86d7a540d
Shu-Ye Lin <67604195+shuyelin06@users.noreply.github.com>
2023-04-08 11:19:50 -0400

    Zoom Slider, Command Box
    Added a zoom slider, command box is finished (can take commands to be parsed).

--- src/graphics/boxes/SliderBox.java
+++ src/graphics/boxes/SliderBox.java
@@ -14,6 +14,9 @@ public class SliderBox extends Box {
 	// Slider 2 - Packet Speed
 	private Slider packetSlider;
 	
+	// Slider 3 - Zoom
+	private Slider zoom; 
+	
 	// Constructor
 	public SliderBox() {
 		tickSlider = new Slider(0, 120f);
@@ -21,10 +24,19 @@ public class SliderBox extends Box {
 		
 		packetSlider = new Slider(0, 3f);
 		packetSlider.setValue(Settings.Packet_Speed);
+		
+		zoom = new Slider(1f, 15f);
+		zoom.setValue(Settings.Pixels_Per_Unit);
 	}
 	
 	// Initialize: Sets the Sizes of the Sliders
 	public void initialize() {
+		zoom
+			.setX(centerX)
+			.setY(centerY - height * 0.05f)
+			.setWidth(width * 0.825f)
+			.setHeight(height * 0.15f);
+		
 		tickSlider
 			.setX(centerX)
 			.setY(centerY + height * 0.15f)
@@ -36,14 +48,18 @@ public class SliderBox extends Box {
 			.setY(centerY + height * 0.35f)
 			.setWidth(width * 0.825f)
 			.setHeight(height * 0.15f);
+		
+		
 	}
 	
 	// Mouse click will move sliders
 	public void mouseClick(float mouseX, float mouseY) {
+		zoom.handleMouse(mouseX, mouseY);
 		tickSlider.handleMouse(mouseX, mouseY);
 		packetSlider.handleMouse(mouseX, mouseY);
 		
 		// Update Setting Variables
+		Settings.Pixels_Per_Unit = zoom.getValue();
 		Settings.Ticks_Per_Second = tickSlider.getValue();
 		Settings.Packet_Speed = packetSlider.getValue();
 	}
@@ -51,6 +67,11 @@ public class SliderBox extends Box {
 	// Draw Method
 	public void draw(Graphics g) {
 		super.draw(g);
+		
+		g.setColor(Color.black);
+		g.drawString("Zoom", centerX - g.getFont().getWidth("Zoom") / 2, 
+				centerY);
+		zoom.draw(g);
 		
 		g.setColor(Color.black);
 		g.drawString("Ticks / Second", centerX - g.getFont().getWidth("Ticks / Second") / 2, 
