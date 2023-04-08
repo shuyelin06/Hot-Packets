df4bec11d29cc839d5b7c11853b245255a12713a
Shu-Ye Lin <67604195+shuyelin06@users.noreply.github.com>
2023-04-08 12:25:19 -0400

    Revert "natRules"
    This reverts commit 808e40988ddee7847b6b10ef3497ffca51a6a798.

--- src/core/protocols/NatRule.java
+++ src/core/protocols/NatRule.java
@@ -1,33 +1,5 @@
 package core.protocols;
 
-import core.objects.Device;
-
 public class NatRule extends Rule {
-	public enum RuleType {PREROUTING, POSTROUTING};
 	
-	// Rule can be either PREOUTING or POSTROUTING
-	private RuleType routingRule;
-	
-	// Holds the device that is the sender of the packet
-	private int[] SNAT;	
-	// Holds the device that is the reciever of the packet
-	private int[] DNAT;
-	
-	public NatRule(RuleType rule, int[] sourceIP, Device destination) {
-		routingRule = rule;
-		SNAT = sourceIP;
-		DNAT = destination.getIP();
-	}
-	
-	public RuleType getRule() {
-		return routingRule;
-	}
-	
-	public int[] getSnat() {
-		return SNAT;
-	}
-	
-	public int[] getDnat() {
-		return DNAT;
-	}
 }
