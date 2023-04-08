e6a19a44b8339f9d5033136ae4bf707543a61f00
Skyen09 <brianxie1298@gmail.com>
2023-04-07 22:12:31 -0400

    Update Device.java

--- src/core/objects/Device.java
+++ src/core/objects/Device.java
@@ -30,10 +30,44 @@ public class Device extends NetworkObject {
 		
 	}
 	
+	//
+	public Device nextDevice(Device destination) {
+	ArrayList<Device> potential = new ArrayList<Device>();
+	ArrayList<Device> traveled = new ArrayList<Device>();
+	traveled.add(this);
+		for(int i = 0; i < connections.size(); i++) {
+			if(nextDeviceAux(connections.get(i), destination, traveled)) {
+				potential.add(connections.get(i));
+			}
+		}
+		return potential.get((int) ((Math.random() * (2 - 0)) + 0));
+	}
+	
+	private boolean nextDeviceAux(Device curr, Device destination, ArrayList<Device> traveled) {
+		boolean notTraveled = true;		
+		for(int i = 0; i < curr.connections.size(); i++) {
+			for(int j = 0; j < traveled.size(); j++) {
+				if (traveled.get(j).equals(curr.connections.get(i))) {
+					notTraveled = false;
+				}
+			}
+			if(curr.connections.get(i).equals(destination) && notTraveled) {
+				return true;
+			} else if (!notTraveled){
+				continue;
+			} else {
+				traveled.add(this);
+				nextDeviceAux(curr.connections.get(i), destination, traveled);
+			}
+		}
+	}
+	
 	// Draw Method
 	@Override
 	public void draw(Graphics g) {
 		
 	}
+	
+}{
 	
 }
